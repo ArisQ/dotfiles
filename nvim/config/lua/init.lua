5fa526c7fbc7ca602f15a5ca79e98aaa08297887
ArisQ <qiaoli.pg@gmail.com>
2023-04-11 22:56:03 +0800

    telescope key bindings

--- nvim/config/lua/init.lua
+++ nvim/config/lua/init.lua
@@ -1,9 +1,22 @@
 require 'plugins'
 
+local telescope_builtin = require 'telescope.builtin'
 local wk = require 'which-key'
 wk.register({
     e = {'<cmd>NvimTreeToggle<cr>', 'File Explorer'},
-    fl = {'<cmd>NvimTreeToggle<cr>', 'File Explorer'},
+    -- fl = {'<cmd>NvimTreeToggle<cr>', 'File Explorer'},
+    f = {
+        name = 'Telescope Find',
+        -- f = {'<cmd>Telescope find_files<cr>', 'Find Files'},
+        -- g = {'<cmd>Telescope live_grep<cr>', 'Live Grep'},
+        -- b = {'<cmd>Telescope buffers<cr>', 'Buffers'},
+        -- h = {'<cmd>Telescope help_tags<cr>', 'Help Tags'},
+        f = {telescope_builtin.find_files, 'Find Files'},
+        g = {telescope_builtin.live_grep, 'Live Grep'},
+        b = {telescope_builtin.buffers, 'Buffers'},
+        h = {telescope_builtin.help_tags, 'Help Tags'},
+        -- t = {telescope_builtin.treesitter, 'Tree Sitter'},
+    },
     p = {
         name = 'Packer',
         i = {'<cmd>PackerInstall<cr>', 'Install'},
