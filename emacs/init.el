7f6e3bd611961ca05784a30ce40b19e4dc89219b
qiaoli <qiaoli@bilibili.com>
2024-03-07 21:32:54 +0800

    emacs org mode

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,5 @@
-
+(setq auth-sources '("~/.authinfo"))
+(setq mac-command-modifier 'meta)
 
 ;; C-x C-e to execute sexp
 
@@ -14,15 +15,24 @@
 
 ;; (set-face-attribute 'default nil :font "Fira Code Retina" :height 280)
 ;; (set-face-attribute 'default nil :font "WenQuanYi Zen Hei Mono" :height 160)
+(set-face-attribute 'default nil :font "FiraCode Nerd Font" :height 150)
 ;; (add-to-list 'default-frame-alist '(font . "Yahei Consolas Hybrid-12"))
 ;; (add-to-list 'default-frame-alist '(font . "WenQuanYi Zen Hei"))
-(add-to-list 'default-frame-alist '(font . "WenQuanYi Zen Hei Mono-16"))
+;; (add-to-list 'default-frame-alist '(font . "WenQuanYi Zen Hei Mono-16"))
 ;; (add-to-list 'default-frame-alist '(font . "YaHei Consolas Hybrid-16"))
+
+;; enable by `M-x variable-pitch-mode`
+(set-face-attribute 'fixed-pitch nil :font "FiraCode Nerd Font" :height 180)
+(set-face-attribute 'variable-pitch nil :font "YaHei Consolas Hybrid" :height 200 :weight 'regular)
+
+
+; (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Themes
 ;; https://emacsthemes.com/
 ;;
 ;; (load-theme 'tango-dark)
+;; (load-theme 'wombat)
 ;; (load-theme 'doom-solarized-dark)
 ;; (load-theme 'doom-tomorrow-night)
 ;; (load-theme 'doom-monokai-classic) ;; 背景偏黄
@@ -143,8 +153,8 @@
 (general-create-definer leader-key :prefix "C-c")
 
 
-;; (use-package evil
-;;   :init (evil-mode 0))
+(use-package evil
+  :init (evil-mode 0))
 ;; (use-package evil-collection
 ;;   :after evil
 ;;   :config (evil-collection-init))
@@ -182,6 +192,68 @@
   :config
   (add-to-list 'forge-alist '("git.bilibili.co" "git.bilibili.co/api/v4" "git.bilibili.co" forge-gitlab-repository)))
 
+(defun aq/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+;;  (setq evil-auto-indent nil))
+;;  (auto-fill-mode 0)
+
+
+(use-package org
+;;  :hook (org-mode . aq/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+  (setq	org-hide-emphasis-markers t)
+
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+
+  (setq org-todo-keywords
+       '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+	 (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+  ;; https://github.com/daviwil/emacs-from-scratch/blob/5e1f99448e32852277e2d274ce2057d55b8c7aaf/init.el#L300
+
+  (setq org-agenda-files '("~/Nextcloud/OrgMode/wiki/editors/emacs/emacs-from-scratch.org")))
+
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+;; org mode heading font size
+(dolist (face '((org-level-1 . 1.2)
+		(org-level-2 . 1.1)
+		(org-level-3 . 1.05)
+		(org-level-4 . 1.0)
+		(org-level-5 . 1.1)
+		(org-level-6 . 1.1)
+		(org-level-7 . 1.1)
+		(org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font "FiraCode Nerd Font" :weight 'regular :height (cdr face)))
+
+;; Ensure that anything that should be fixed-pitch in Org files appears that way
+(set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+(set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+(set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
+(set-face-attribute 'line-number nil :inherit 'fixed-pitch)
+(set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch)
+
+;; org mode 居中显示
+(defun aq/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+	visual-fill-column-center-text t)
+  (visual-fill-column-mode))
+(use-package visual-fill-column
+  :defer t
+  :hook (org-mode . aq/org-mode-visual-fill))
 
 
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
